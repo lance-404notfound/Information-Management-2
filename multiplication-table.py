f378554e4dda1b5a2e88a5441dc75b6df9ddd7db
lance-404notfound <lanceleonen@gmail.com>
2025-09-03 23:10:29 +0800

    Add Multiplication Program

---
+++ multiplication-table.py
@@ -0,0 +1,12 @@
+rows = int(input("Enter row: "))
+cols = int(input("Enter col: "))
+search = int(input("Search: "))
+
+for row in range(1, rows + 1):
+    for col in range(1, cols + 1):
+        value = row * col
+        if value == search:
+            print("[" + str(value) + "]", end=" ")
+        else:
+            print(value, end=" ")
+    print()
